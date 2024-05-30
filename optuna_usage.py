90583eeefe8f995d46bf2d8ccb47785aa277943e
dmitriy <dmitriy29062004@gmail.com>
2024-05-30 15:40:40 +0300

    add optuna_usage

---
+++ optuna_usage.py
@@ -0,0 +1,13 @@
+import optuna
+
+
+def objective(trial):
+    x = trial.suggest_float('x', -100, 100)
+    return (x - 2) ** 2
+
+
+optuna.logging.set_verbosity(optuna.logging.WARNING)
+study = optuna.create_study()
+study.optimize(objective, n_trials=1000)
+
+print(study.best_params)
